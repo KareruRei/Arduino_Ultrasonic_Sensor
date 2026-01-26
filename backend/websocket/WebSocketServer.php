41c8fa3fdf660689c86b8564497c1f42a6634a83
unknown <christianagot11@gmail.com>
2026-01-26 23:47:01 +0800

    removed datetime from data sent to browsers. included datetime in logs

--- backend/websocket/WebSocketServer.php
+++ backend/websocket/WebSocketServer.php
@@ -9,7 +9,7 @@ class WebSocketServer implements MessageComponentInterface {
 
     public function onOpen(ConnectionInterface $conn) {
         $this->clients[$conn->resourceId] = $conn;
-        echo "New client has connected: {$conn->resourceId}\n";
+        echo "{date('Y-m-d H:i:s')} - New client has connected: {$conn->resourceId}\n";
     }
 
     public function onMessage(ConnectionInterface $from, $data) {
@@ -22,9 +22,6 @@ class WebSocketServer implements MessageComponentInterface {
         }
 
         if ($from === $this->arduino) {
-            $now = date('Y-m-d H:i:s');
-            $data = substr_replace($data, ',"datetime":"' . $now . '"}', -1);
-
             foreach ($this->clients as $client) {
                 $client->send($data);
             }
@@ -34,12 +31,12 @@ class WebSocketServer implements MessageComponentInterface {
     public function onClose(ConnectionInterface $conn) {
         if ($conn === $this->arduino) {
             $this->arduino = null;
-            echo "Arduino connection: {$conn->resourceId} disconnected.\n";
+            echo "{date('Y-m-d H:i:s')} - Arduino connection: {$conn->resourceId} disconnected.\n";
             return;
         }
 
         unset($this->clients[$conn->resourceId]);
-        echo "Client: {$conn->resourceId} disconnected.\n";
+        echo "{date('Y-m-d H:i:s')} - Client: {$conn->resourceId} disconnected.\n";
     }
 
     public function onError(ConnectionInterface $conn, \Exception $e) {
