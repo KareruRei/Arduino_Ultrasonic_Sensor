a67658d633a87d92effb3594556f1198c539b2f4
unknown <christianagot11@gmail.com>
2026-01-28 14:32:27 +0800

    added log post request to ws server every 10s. added amp/http for fast post requests

--- backend/websocket/WebSocketServer.php
+++ backend/websocket/WebSocketServer.php
@@ -1,11 +1,17 @@
 <?php
 
-use Ratchet\MessageComponentInterface;
+use Amp\Http\Client\HttpClient;
+use Ratchet\WebSocket\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
+use Ratchet\RFC6455\Messaging\MessageInterface;
+use Amp\Http\Client\HttpClientBuilder;
+use Amp\Http\Client\Request;
 
 class WebSocketServer implements MessageComponentInterface {
     protected ?ConnectionInterface $arduino = null;
     protected array $clients = [];
+    private HttpClient $http_client = new HttpClientBuilder()::buildDefault();
+    private int $lastLog = 0;
 
     public function onOpen(ConnectionInterface $conn) {
         $this->clients[$conn->resourceId] = $conn;
@@ -13,20 +19,43 @@ class WebSocketServer implements MessageComponentInterface {
         echo "$now - New client has connected: {$conn->resourceId}\n";
     }
 
-    public function onMessage(ConnectionInterface $from, $data) {
-        $decoded = json_decode($data, true) ?? [];
+    public function onMessage(ConnectionInterface $from, MessageInterface $data) {
+        if (!$data->isBinary()) {
+            $decoded = json_decode($data, true) ?? [];
 
-        if (($decoded['role'] ?? '') === 'arduino') {
-            $this->arduino = $from;
-            unset($this->clients[$from->resourceId]);
+            if (($decoded['role'] ?? '') === 'arduino') {
+                $this->arduino = $from;
+                unset($this->clients[$from->resourceId]);
+            }
             return;
         }
+        else if ($from !== $this->arduino) return;
 
-        if ($from === $this->arduino) {
-            foreach ($this->clients as $client) {
-                $client->send($data);
-            }
+        foreach ($this->clients as $client) {
+            $client->send($data);
         }
+
+        if (time() - $this->lastLog < 60*10) return;
+        $this->lastLog = time();
+
+        $payload = $data->getPayload();
+
+        $angle = ord($payload[0]);
+        $distance = (ord($payload[1]) << 8) | ord($payload[2]);
+        $locked = ord($payload[3]) == 1;
+
+        $log_request = new Request('http://localhost/post-logs.php', 'POST');
+
+        $request_payload = [
+            'angle' => $angle,
+            'distance' => $distance,
+            'locked' => $locked
+        ];
+
+        if ($locked) $request_payload['description'] = 'Intruder detected';
+
+        $log_request->setBody(json_encode($request_payload));
+        $this->http_client->request($log_request);
     }
 
     public function onClose(ConnectionInterface $conn) {
